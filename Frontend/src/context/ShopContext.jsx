8a1c5c8f91ff25d6c289c7a616548b91c48d57aa
kiran shahi <shahikiran575@gmail.com>
2026-01-04 21:23:14 +0545

    managing access token

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,17 +1,21 @@
 import React, { use, useEffect, useState } from "react";
-import { products } from "../assets/assets";
+
 import { createContext } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const currency = "$";
   const delivery_fee = 10;
   const [cartitems, setCartitems] = useState({});
   const navigate = useNavigate();
+  const [products, setProducts] = useState([]);
+  const [token, setToken] = useState({});
 
   const addToCart = async (itemId, size) => {
     if (!size) {
@@ -69,10 +73,38 @@ const ShopContextProvider = (props) => {
     }
     return totalAmount;
   };
+  const getProductsData = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list");
+
+      if (response.data.success) {
+        setProducts(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getProductsData();
+  }, []);
+
+  useEffect(() => {
+    if (!token && localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
+    }
+  }, []);
 
   const value = {
     currency,
+    backendUrl,
     products,
+    token,
+    setToken,
+
     delivery_fee,
     search,
     setSearch,
