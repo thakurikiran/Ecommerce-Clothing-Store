f806897c9a8c8b0204056de1b97559a099887dd5
kiran shahi <shahikiran575@gmail.com>
2026-01-04 21:20:30 +0545

    feat: connecting Login with server

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,10 +1,55 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { toast } from "react-toastify";
 
 const Login = () => {
-  const [currentState, setCurrentState] = useState("Sign Up");
+  const [currentState, setCurrentState] = useState("Login");
+  const { backendUrl, token, setToken, navigate } = useContext(ShopContext);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const onSubmitHandler = async (e) => {
     e.preventDefault();
+    try {
+      if (currentState === "Sign Up") {
+        const response = await axios.post(backendUrl + "/api/user/register", {
+          name,
+          email,
+          password,
+        });
+
+        if (response.data.success) {
+          setToken(response.data.token);
+          localStorage.setItem("token", response.data.token);
+        } else {
+          toast.error(response.data.message);
+        }
+      } else {
+        const response = await axios.post(backendUrl + "/api/user/login", {
+          email,
+          password,
+        });
+
+        if (response.data.success) {
+          setToken(response.data.token);
+          localStorage.setItem("token", response.data.token);
+        } else {
+          toast.error(response.data.message);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
   };
+
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  }, [token]);
+
   return (
     <form
       onSubmit={onSubmitHandler}
@@ -14,11 +59,13 @@ const Login = () => {
         <p className="prata-regular text-3xl"> {currentState}</p>
         <hr className="border-none h-[1.5px ] w-8 bg-gray-800" />
       </div>
-      {currentState == "Login" ? (
+      {currentState === "Login" ? (
         ""
       ) : (
         <input
           type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
           className="w-full px-3 py-2 border border-gray-800 "
           placeholder="Name"
           required
@@ -26,19 +73,23 @@ const Login = () => {
       )}
       <input
         type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
         className="w-full px-3 py-2 border border-gray-800 "
         placeholder="Email"
         required
       />
       <input
         type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
         className="w-full px-3 py-2 border border-gray-800 "
         placeholder="password"
         required
       />
       <div className=" w-full text-sm mt-[-8px] flex justify-between">
         <p className="cursor-pointer">Forget Your Password? </p>
-        {currentState == "Login" ? (
+        {currentState === "Login" ? (
           <p
             onClick={() => setCurrentState("Sign Up")}
             className="cursor-pointer"
@@ -55,7 +106,7 @@ const Login = () => {
         )}
       </div>
       <button className="bg-black text-white font-light px-8 py-2 mt-4 cursor-pointer">
-        {currentState == "Login" ? "Sign In" : "Sign Up"}
+        {currentState === "Login" ? "Sign In" : "Sign Up"}
       </button>
     </form>
   );
