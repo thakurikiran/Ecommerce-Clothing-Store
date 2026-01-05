a169e3d58d2dac170d91af35b11f183596eff501
kiran shahi <shahikiran575@gmail.com>
2026-01-05 21:22:21 +0545

    adding route api end point  to the cart

---
+++ Backend/routes/cartRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import {
+  addToCart,
+  getUserCart,
+  updateCart,
+} from "../controllers/cartController";
+
+const cartRouter = express.Router();
+
+cartRouter.post("/get", getUserCart);
+cartRouter.post("/add", addToCart);
+cartRouter.post("/update", updateCart);
+
+export default cartRouter;
