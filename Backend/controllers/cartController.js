712583d592584dc5b673c80d518af0f27dbec065
kiran shahi <shahikiran575@gmail.com>
2026-01-05 21:20:34 +0545

    Creating controller for add update and getuser cart

---
+++ Backend/controllers/cartController.js
@@ -0,0 +1,10 @@
+//add Products to user Cart
+const addToCart = async (req, res) => {};
+
+//update user Cart
+const updateCart = async (req, res) => {};
+
+//get user cart data
+const getUserCart = async (req, res) => {};
+
+export { addToCart, updateCart, getUserCart };
